45fe010ad7e377a3498f002b70b9890fb8a9621b
Artur <morozarturv@gmail.com>
2013-02-15 16:38:39 +0200

    first went

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+//logo
+var logo_width = $('.logo').width();
+var logo_height = $('.logo').height();
+$('.logo').css({'margin-top':-logo_height/2-100, 'margin-left':-logo_width/2});
+
+
+});
